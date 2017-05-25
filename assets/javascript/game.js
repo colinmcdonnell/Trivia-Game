8ed4d831e36bbff7fc9db789f942e3ee69bd5ea3
Colin McDonnell <colinmcdonnell@colins-mbp.attlocal.net>
2017-05-25 14:34:01 -0500

    trvia-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@
 
 $('#startButton').on( {
 	'click': function() {
+
+    $('#questionsBox').fadeIn(2000);
     
     var count = 30;
 
@@ -33,54 +35,60 @@ $('.correct').click(function() {
 
 $("#question1").click(function(){
     $('#question1').text('CORRECT!')
-    $('.wrong1').text('WRONG!')
+    $('.wrong1').text('')
 });
 
 $("#question2").click(function(){
     $('#question2').text('CORRECT!')
-    $('.wrong2').text('WRONG!')
+    $('.wrong2').text('')
 });
 
 $("#question3").click(function(){
     $('#question3').text('CORRECT!')
-    $('.wrong3').text('WRONG!')
+    $('.wrong3').text('')
 });
 
 $("#question4").click(function(){
     $('#question4').text('CORRECT!')
-    $('.wrong4').text('WRONG!')
+    $('.wrong4').text('')
 });
 
 $("#question5").click(function(){
     $('#question5').text('CORRECT!')
-    $('.wrong5').text('WRONG!')
+    $('.wrong5').text('')
 });
 
 $("#question6").click(function(){
     $('#question6').text('CORRECT!')
-    $('.wrong6').text('WRONG!')
+    $('.wrong6').text('')
 });
 
 $(".wrong1").click(function(){
     $('.wrong1').text('WRONG!')
+    $('#question1').text('')
 });
 
 $(".wrong2").click(function(){
     $('.wrong2').text('WRONG!')
+    $('#question2').text('')
 });
 
 $(".wrong3").click(function(){
     $('.wrong3').text('WRONG!')
+    $('#question3').text('')
 });
 
 $(".wrong4").click(function(){
     $('.wrong4').text('WRONG!')
+    $('#question4').text('')
 });
 
 $(".wrong5").click(function(){
     $('.wrong5').text('WRONG!')
+    $('#question5').text('')
 });
 
 $(".wrong6").click(function(){
     $('.wrong6').text('WRONG!')
+    $('#question6').text('')
 });
